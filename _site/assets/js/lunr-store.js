e47f54fd92c459b5317a29b59be7b7c55c98324f
Tyler Pearson <tdpearson@ou.edu>
2026-04-08 11:02:18 -0500

    Initial CollectionBuilder setup

---
+++ _site/assets/js/lunr-store.js
@@ -0,0 +1,29 @@
+var store = [{  
+"title": "Administration Building, University of Idaho, No. 30","date": "1910","creator": "Pacific Photo Co.","description": "Example locally hosted image item. Photographic postcard of the University of Idaho administration building in Moscow, Idaho.","subject": "universities; buildings; campuses; picture postcards","location": "Moscow, Idaho", 
+"id": "demo_001.html"
+
+},{  
+"title": "Spokane County Court House, Spokane, Washington","date": "1912-09-08","creator": "Spokane Post Card Co.","description": "Example locally hosted PDF item. Postcard is of the Spokane County Courthouse in Spokane, Washington. Postmark 9/8/1912, Spokane, WA. Destination San Francisco, CA.","subject": "public buildings; county courthouses; trees; picture postcards","location": "Spokane, Washington", 
+"id": "demo_002.html"
+
+},{  
+"title": "Good News – Power (radio episode)","date": "1947","creator": "Frank B. Robinson","description": "Example locally hosted audio item. Psychiana radio program episode begins with assurance that there is a Power that can get people what they desire. Robinson then discusses his early life failures and childhood seeking of some force that could turn his failures into success. He asserts the God-Power can turn our failures into success and bring us material and spiritual abundance.","subject": "Frank B. Robinson; Failure; Success; Material Abundance","location": "Moscow, Idaho", 
+"id": "demo_003.html"
+
+},{  
+"title": "University of Idaho vs. University of Southern California (Football), 10/30/1925","date": "1925-10-30","creator": "Vandal Athletics","description": "Example YouTube video item. Idaho Football vs. University of Southern California 10/30/1925 at Neale Stadium in Moscow, ID. Score: 7 - 51 (L).","subject": "American Football","location": "Moscow, Idaho", 
+"id": "demo_004.html"
+
+},{  
+"title": "Interview with K. Silem Mohammad","date": "2014-06-16","creator": "Devin Becker","description": "Example Vimeo video item. Oral history interview with poet K. Silem Mohammad discussing recent changes in writing practices that occurred due to the advent of the computer and the arrival of the digital age.","subject": "Poetry","location": "Ashland, OR", 
+"id": "demo_005.html"
+
+},{  
+"title": "Ford pumper used for slash burning control","date": "1932-09-01","creator": "Thompson, J. B.","description": "Example image item externally hosted. Ford pumper used for slash burning control. September 1932. From the photo series depicting the broadcast burn at the mouth of Benton Creek.","subject": "forestry","location": "Bonner County;Priest River Experimental Forest;Benton Creek", 
+"id": "demo_006.html"
+
+},{  
+"title": "Influence of Fishway Placement on Fallback of Adult Salmon at the Bonneville Dam on the Columbia River","date": "2003","creator": "Reischel, T.S.; Bjornn, T.C.","description": "Example metadata-only record with link to external source. Journal article.","subject": "fisheries management","location": "Bonneville Dam", 
+"id": "demo_007.html"
+
+}];
